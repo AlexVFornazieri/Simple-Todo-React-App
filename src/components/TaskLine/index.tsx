12a22adfc8b83d7a5ea8f55ed552246fc9f4942d
Alex V. Fornazieri <alex@abelha.network>
2022-11-26 17:11:08 -0300

    Checking n adding itens

--- src/components/TaskLine/index.tsx
+++ src/components/TaskLine/index.tsx
@@ -1,29 +1,50 @@
 import { TodoItem } from '@/models/TodoItem'
-import { Checkbox, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField } from '@mui/material'
+import { Delete } from '@mui/icons-material'
+import {
+  Checkbox,
+  IconButton,
+  ListItem,
+  ListItemSecondaryAction,
+  TextField,
+} from '@mui/material'
+
+import styles from './styles.module.scss'
 
 interface TaskLineProps {
   item: TodoItem
   labelId: string
   handleToggle: (item: TodoItem) => () => void
+  updateText: (item: TodoItem, text: string) => void
 }
 
-export const TaskLine = ({ item, labelId, handleToggle }: TaskLineProps) => {
+export const TaskLine = ({ item, labelId, handleToggle, updateText }: TaskLineProps) => {
   return (
   <ListItem>
-    <ListItemButton role={undefined} onClick={handleToggle(item)} dense>
-      <ListItemIcon>
-        <Checkbox
-          edge="start"
-          checked={item.done}
-          tabIndex={-1}
-          disableRipple
-          inputProps={{ 'aria-labelledby': labelId }}
-        />
-      </ListItemIcon>
-      <ListItemText>
-        <TextField />
-      </ListItemText>
-    </ListItemButton>
+    <Checkbox
+      edge="start"
+      onClick={handleToggle(item)}
+      checked={item.done}
+      tabIndex={-1}
+      disableRipple
+      inputProps={{ 'aria-labelledby': labelId }}
+    />
+    {item.editing && <>
+    <TextField
+    variant="standard"
+    placeholder="Whats need to be done?"
+    onChange={(e) => {
+      updateText(item, e.target.value)
+    }}
+    className={`${item.done ? styles.listChecked : ''} ${styles.textField}`}
+    />
+      <ListItemSecondaryAction>
+    <IconButton>
+      <Delete
+      color="error"
+      />
+    </IconButton>
+    </ListItemSecondaryAction>
+    </>}
   </ListItem>
   )
 }
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,6 +2,7 @@ import List from '@mui/material/List'
 import { v1 as uuid } from 'uuid'
 import { useState } from 'react'
 import { TodoItem } from '@/models/TodoItem'
+import { Button } from '@mui/material'
 import { TaskLine } from '../TaskLine'
 
 const item = (): TodoItem => ({ text: '', done: false, uuid: uuid(), editing: true })
@@ -9,28 +10,48 @@ const item = (): TodoItem => ({ text: '', done: false, uuid: uuid(), editing: tr
 export const TodoList = () => {
   const [items, setItems] = useState<TodoItem[]>([item()])
 
+  const updateItem = (item: TodoItem) => {
+    const index = items.indexOf(item)
+    if (index === -1) {
+      return
+    }
+    const newItems = [...items]
+    newItems[index] = item
+    setItems(newItems)
+  }
+
   const newItem = () => {
     setItems([...items, item()])
   }
 
   const handleToggle = (item: TodoItem) => () => {
-    const index = items.indexOf(item)
-    const newItems = [...items]
-    if (index !== -1 && !item.editing) {
-      newItems[index].done = !item.done
-    }
-    setItems(newItems)
+    item.done = !item.done
+    updateItem(item)
+  }
+
+  const updateText = (item: TodoItem, text: string) => {
+    item.text = text
+    updateItem(item)
   }
 
   return (
+    <div>
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {items.map((item) => {
         const labelId = `checkbox-list-label-${item.uuid}`
 
         return (
-          <TaskLine key={labelId} labelId={labelId} item={item} handleToggle={handleToggle}/>
+          <TaskLine
+          key={labelId}
+          labelId={labelId}
+          item={item}
+          handleToggle={handleToggle}
+          updateText={updateText}
+          />
         )
       })}
     </List>
+    <Button onClick={newItem}>Adicionar task</Button>
+    </div>
   )
 }
